a26c50fe320d92e1777aa897ff6fd948d59fbf32
dmandevv <dmandevv@hotmail.com>
2026-03-06 18:11:48 -0800

    Import and package edits

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { login, register, createCheckoutSession } = require('#api/index').controllers.UserController;
-const { authorizeUserMiddleware } = require('#api/index').middleware.auth;
-const { connectDB } = require('#api/index').middleware.database;
+const { login, register, createCheckoutSession } = require('#controllers/userController');
+const { authorizeUserMiddleware } = require('#middleware/auth');
+const { connectDB } = require('#middleware/database');
 
 router.post('/register', connectDB, register);
 router.post('/login', connectDB, login);
